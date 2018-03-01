3973df802b24f910b306f47e86fd896a7f457619
Huckleberry Febbo <febbo@umich.edu>
2018-02-28 19:49:03 -0500

    Revert "Adds car_demo to Autoware"

--- ros/src/util/packages/runtime_manager/scripts/gazebo.sh
+++ ros/src/util/packages/runtime_manager/scripts/gazebo.sh
@@ -1,20 +1,10 @@
 #!/bin/sh
 
-if [ "$1" = "CarDemo" ]
-then
-  roslaunch car_demo demo.launch
-elif [ "$1" = "CatVechicle" ]
-then
-  roslaunch catvehicle catvehicle_skidpan.launch &
-  gzclient &
-  roslaunch point_cloud_converter point_cloud_converter.launch &
-  roslaunch laser_scan_converter laser_scan_converter.launch &
-  roslaunch twist_cmd_converter twist_cmd_converter.launch &
-elif [ "$1" = "TestMap1" ]
-then
-  #this is to be implemented
-  gazebo
-fi
+roslaunch catvehicle catvehicle_skidpan.launch &
+gzclient &
+roslaunch point_cloud_converter point_cloud_converter.launch &
+roslaunch laser_scan_converter laser_scan_converter.launch &
+roslaunch twist_cmd_converter twist_cmd_converter.launch &
 
 while :; do sleep 10; done
 
